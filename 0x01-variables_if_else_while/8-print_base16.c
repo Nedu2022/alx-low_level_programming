1ee9fa0f7227be31a20961cef4f0f73999f7b448
Nedu2022 <nwabuokeichinedu00@gmail.com>
2022-09-10 00:56:52 +0100

    Final Task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 /**
-* main - print if the bumber is positive, zero or negative
+* main - print if the number is positive, zero or negative
 *
 * Description: using the main function
 * this program prints "Programming is positive, zero or negative
 * Return: 0
 */
-int main(void)
+int main (void)
 {
 char c;
 for (c = '0'; c <= '9'; c++)
 {
-	putchar(c);
+	putchar (c);
 }
 for (c = 'a'; c <= 'f'; c++)
 {
-	putchar(c);
+	putchar (c);
 }
-putchar('\n');
+putchar ('\n');
 return(0);
 }
