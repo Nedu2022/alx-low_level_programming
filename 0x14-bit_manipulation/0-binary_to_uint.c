37151d7a524d0916accbb42b215934ab73cedbde
Nedu2022 <nwabuokeichinedu00@gmail.com>
2022-10-27 22:51:17 +0100

    Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,5 @@
+#include <stddef.h>
 #include "main.h"
-/**
- * binary_to_uint - converts a binary to an unsigned int
- * @b: the binary number as a string
- *
- * Return: the converted value
- */
-unsigned int binary_to_uint(const char *b)
-{
-unsigned int decimal = 0;
-int str_len = 0, base = 1;
-
-if (!check_valid_string(b))
-return (0);
-
-while (b[str_len] != '\0')
-str_len++;
-
-while (str_len)
-{
-decimal += ((b[str_len - 1] - '0') * base);
-base *= 2;
-str_len--;
-}
-return (decimal);
-}
 
 /**
  * check_valid_string - checks if a string has only 0's and 1's
@@ -33,14 +9,40 @@ return (decimal);
  */
 int check_valid_string(const char *b)
 {
-if (b == NULL)
-return (0);
+	if (b == NULL)
+		return (0);
+	while (*b)
+	{
+		if (*b != '1' && *b != '0')
+			return (0);
+		b++;
+	}
 
-while (*b)
-{
-if (*b != '1' && *b != '0')
-return (0);
-b++;
+	return (1);
 }
-return (1);
+
+
+/**
+ * binary_to_uint - converts a binary to an unsigned int
+ * @b: the binary number as a string
+ *
+ * Return: the converted value
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	int str_len = 0, base = 1;
+
+	if (!check_valid_string(b))
+		return (0);
+	while (b[str_len] != '\0')
+		str_len++;
+	while (str_len)
+	{
+		decimal += ((b[str_len - 1] - '0') * base);
+		base *= 2;
+		str_len--;
+	}
+
+	return (decimal);
 }
