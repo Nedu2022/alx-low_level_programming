cb544484ddc17fdd57529fe90c386af9c769a787
Nedu2022 <nwabuokeichinedu00@gmail.com>
2022-10-28 21:18:37 +0100

    putchar

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-/*Don't delete _putchar, its used for testing*/
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
